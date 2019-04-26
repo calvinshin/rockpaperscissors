847951c841de10ed8e669747b07ad65642f94327
Calvin Shin <calvin.shin@gmail.com>
2019-04-25 18:46:26 -0700

    Created a new version of the game that makes it object oriented and uses better logical statements

---
+++ javascript old.js
@@ -0,0 +1,87 @@
+// {/* <script type="text/javascript"> */}
+
+// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+var computerChoices = ["r", "p", "s"];
+
+// Score needs to be saved outside of the event so that the score is not reset after each game
+var userScore = 0
+var computerScore = 0
+
+function textify(character) {
+    if (character === "r") {
+        return "rock";
+    }
+    else if (character === "p") {
+        return "paper";
+    }
+    else if (character === "s") {
+        return "scissors";
+    }
+    else {
+        return "NA"
+    }
+}
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(keypress) {
+
+  // Determines which key was pressed.
+  var userGuess = keypress.key;
+
+  // Randomly chooses a choice from the options array. This is the Computer's guess.
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+//   Attempt at a function to change to make userGuess and computerGuess a text value
+    // function textify(userGuess) {
+    //     if (userGuess === "r") {
+    //         var userGuessCapital = "rock"
+    //     }
+    // }
+
+
+  // Alerts the key the user pressed (userGuess) IF a value of r, p, or s is selected
+  if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
+    alert("You chose: " + textify(userGuess) + "\r\n" + "The computer chose: " + textify(computerGuess));
+
+    console.log(textify(userGuess));
+
+    var outcome = userGuess + computerGuess;
+
+    // alert("You tie! \r\nhaha");
+    // use \r\n for when you want a line break in an alert
+
+    if (userGuess === computerGuess) {
+      alert("You tie!");
+      document.getElementById("outcomeElement").innerHTML = (outcome + " | tie");
+      console.log(outcome + " | tie | " + userScore + " | " + computerScore);
+    }
+    else if (outcome === "pr" || outcome === "rs" || outcome === "sp"){
+      alert("You win!");
+      userScore = userScore + 1;
+      document.getElementById("outcomeElement").innerHTML = (outcome + " | win");
+      console.log(outcome + " | win | " + userScore + " | " + computerScore);
+      document.getElementById("userScoreElement").innerHTML = userScore;
+    }
+    else if (outcome === "rp" || outcome === "sr" || outcome === "ps"){
+      alert("You lose!")
+      computerScore = computerScore + 1;
+      document.getElementById("outcomeElement").innerHTML = (outcome + " | lose");
+      console.log(outcome + " | lose | " + userScore + " | " + computerScore);
+      document.getElementById("computerScoreElement").innerHTML = computerScore;
+    }
+    else {
+      alert("Something broke! :(!")
+    }
+    ;
+
+    // else if {
+    //   alert("You lose!")
+    // }
+  }
+  else {
+      console.log("invalid input");
+  }
+
+};
+
+// </script>
--- javascript.js
+++ javascript.js
@@ -1,87 +1,68 @@
-{/* <script type="text/javascript"> */}
+app = {
+    userScore : 0,
+    computerScore : 0,
+    options : ["Rock", "Paper", "Scissors"],
+    validRPS : ["r", "p", "s"],
+    key : "",
+    keyToOption : function(key) {
+        if(key === "r"){
+            return "Rock"
+        }
+        else if(key === "s"){
+            return "Scissors"
+        }
+        else if(key === "p"){
+            return "Paper"
+        }
+        else{
+            console.log("Error found in logic");
+        }
+    },
+    keyListener : function() {
+        document.addEventListener("keyup", function(keypress) {
+            // Create a variable for a key
+            var key = keypress.key.toLocaleLowerCase()
 
-// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-var computerChoices = ["r", "p", "s"];
+            // check if it's a valid key and then insert it into the RPS game 
+            if(app.validRPS.indexOf(key) > -1) {
+                var userOneGuess = app.keyToOption(key);
+                app.gameEvent(userOneGuess);
+            }
+        })
+    },
+    gameEvent : function(userOneGuess) {
+        // Converts and sets the option of userOne
+        var userOne = userOneGuess
+        // Sets the option of userTwo (or the computer, in this instance)
+        var userTwo = this.computerGuess();
 
-// Score needs to be saved outside of the event so that the score is not reset after each game
-var userScore = 0
-var computerScore = 0
+        console.log("1: " + userOne + " 2: " + userTwo);
 
-function textify(character) {
-    if (character === "r") {
-        return "rock";
-    }
-    else if (character === "p") {
-        return "paper";
-    }
-    else if (character === "s") {
-        return "scissors";
-    }
-    else {
-        return "NA"
+        // determine the winner using integers; 0 is a tie.
+        var winner = this.winner(userOne, userTwo);
+    },
+    // returns a guessed value
+    computerGuess : function() {
+        var guess = this.options[Math.floor( Math.random() * this.options.length)];
+        return guess
+    },
+    winner : function(playerOne, playerTwo) {
+        if(playerOne === playerTwo) {
+            return 0
+        }
+        pOne = this.options.indexOf(playerOne)
+        pTwo = this.options.indexOf(playerTwo);
+
+        if(pOne + 1 === pTwo || pOne - 2 === pTwo){
+            return 2
+        }
+        else{
+            return 1
+        }
+    },
+    initialize : function() {
+        this.keyListener();
     }
 }
 
-// This function is run whenever the user presses a key.
-document.onkeyup = function(keypress) {
-
-  // Determines which key was pressed.
-  var userGuess = keypress.key;
-
-  // Randomly chooses a choice from the options array. This is the Computer's guess.
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-//   Attempt at a function to change to make userGuess and computerGuess a text value
-    // function textify(userGuess) {
-    //     if (userGuess === "r") {
-    //         var userGuessCapital = "rock"
-    //     }
-    // }
-
-
-  // Alerts the key the user pressed (userGuess) IF a value of r, p, or s is selected
-  if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
-    alert("You chose: " + textify(userGuess) + "\r\n" + "The computer chose: " + textify(computerGuess));
-
-    console.log(textify(userGuess));
-
-    var outcome = userGuess + computerGuess;
-
-    // alert("You tie! \r\nhaha");
-    // use \r\n for when you want a line break in an alert
-
-    if (userGuess === computerGuess) {
-      alert("You tie!");
-      document.getElementById("outcomeElement").innerHTML = (outcome + " | tie");
-      console.log(outcome + " | tie | " + userScore + " | " + computerScore);
-    }
-    else if (outcome === "pr" || outcome === "rs" || outcome === "sp"){
-      alert("You win!");
-      userScore = userScore + 1;
-      document.getElementById("outcomeElement").innerHTML = (outcome + " | win");
-      console.log(outcome + " | win | " + userScore + " | " + computerScore);
-      document.getElementById("userScoreElement").innerHTML = userScore;
-    }
-    else if (outcome === "rp" || outcome === "sr" || outcome === "ps"){
-      alert("You lose!")
-      computerScore = computerScore + 1;
-      document.getElementById("outcomeElement").innerHTML = (outcome + " | lose");
-      console.log(outcome + " | lose | " + userScore + " | " + computerScore);
-      document.getElementById("computerScoreElement").innerHTML = computerScore;
-    }
-    else {
-      alert("Something broke! :(!")
-    }
-    ;
-
-    // else if {
-    //   alert("You lose!")
-    // }
-  }
-  else {
-      console.log("invalid input");
-  }
-
-};
-
-// </script>
+app.initialize();
